516fce2961d3c3d251f029d2f672ec90836af935
Nurbolsun <kg.nurbo.97@mail.ru>
2023-12-21 19:14:18 +0600

    requiments

--- urok47/app.py
+++ urok47/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,11 @@ def homepage():
     f = open("goods.txt", "r", encoding="utf-8")
     txt = f.readlines()
     return render_template("index.html", goods = txt)
+
+@app.route("/add/", methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open("goods.txt", "a+", encoding="utf-8")
+    f.write(good + "\n")
+    f.close()
+    return "Инвентарь пополнен!"
