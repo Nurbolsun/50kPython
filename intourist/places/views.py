dc7a44d2409eb13cfaf6e1aed46fe8f195bee790
Nurbolsun <kg.nurbo.97@mail.ru>
2023-12-25 16:54:32 +0600

    mvt

--- intourist/places/views.py
+++ intourist/places/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Place
 # Create your views here.
+
+def places(request):
+    place_obj = Place.objects.all() #selec from *
+    return render(request, 'places.html', {'places' : place_obj})
