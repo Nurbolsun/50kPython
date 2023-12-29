76ff966007541a9e3705c77a7b56bbcd5ba918dc
Nurbolsun <kg.nurbo.97@mail.ru>
2023-12-29 14:54:35 +0600

    places

--- intourist/core/views.py
+++ intourist/core/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "index.html")
+    return render(request, "core/index.html")
 
 def profile(request):
     user = request.user
     profile_obj = user.profile
-    return render(request, "profile.html", {"profile": profile_obj})
+    return render(request, "core/profile.html", {"profile": profile_obj})
--- intourist/places/views.py
+++ intourist/places/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Place
+from .forms import PlaceForm
 # Create your views here.
 
 def places(request):
     place_obj = Place.objects.all() #selec from *
-    return render(request, 'places.html', {'places' : place_obj})
+    return render(request, 'places/places.html', {'places' : place_obj})
+
+def create_place(request):
+    if request.method == "POST":
+        place_form = PlaceForm(request.POST)
+        if place_form.is_valid():
+            place_form.save()
+            return redirect(places)
+    place_form = PlaceForm()
+    return render(request, 'places/form.html', {'place_form': place_form}) 
+
+def place(request, id):
+    place_obj = Place.objects.get(id=id)
+    place_obj.views_count = place_obj.views_count + 1
+    place_obj.save()
+    return render(request, 'places/place.html', {'place_obj': place_obj})
+
+def edit_place(request, id):
+    place_obj = Place.objects.get(id=id)
+    if request.method == "POST":
+        place_form = PlaceForm(data=request.POST, instance=place_obj)
+        place_form.save()
+        return redirect(place, id=id)
+
+    place_form = PlaceForm(instance=place_obj)
+    return render(request, 'places/form.html', {'place_form': place_form})
+
+def delete_place(request, id):
+    place_obj = Place.objects.get(id=id)
+    place_obj.delete()
+    return redirect(places)
